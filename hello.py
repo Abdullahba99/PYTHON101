9fdc6cdfd85496106868003f5d7d81912d699759
Abdullahba99 <abdullahbabouli@gmail.com>
2025-12-06 13:10:24 +0300

    update

--- hello.py
+++ hello.py
@@ -2,3 +2,16 @@ print("welcome to python programming")
 
 grade = float(input("Enter your grade: "))
 print("Your grade is:", grade)
+
+if grade >= 90:
+    print("You received an A.")
+elif grade >= 80:
+    print("You received a B.")                  
+elif grade >= 70:
+    print("You received a C.")
+elif grade >= 60:
+    print("You received a D.")
+else:
+    print("You received an F.")     
+
+
